e32d2d7c0dd5b2785fa255ee84fde8dd786b7ca3
raj <115876011+rajNson@users.noreply.github.com>
2023-07-02 17:52:54 +0530

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     name = models.CharField(max_length=250,unique=True)
     slug = models.SlugField(max_length=250,unique=True)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='category',blank=True)
+    image = models.ImageField(upload_to='product',blank=True)
     class Meta:
         ordering = ('name',)
         verbose_name = 'category'
